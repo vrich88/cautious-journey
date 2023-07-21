89c6cee22d0366f09da061e141658909a07a13b3
dctristero <123331986+dctristero@users.noreply.github.com>
2023-07-20 19:07:35 -0500

    rough draft of mapped subforum cards

---
+++ client/src/pages/testpages/Dannytest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UFOcards from '../../components/subForumCards/UFOcards';
+
+function Dannytest() {
+  return (
+    <div>
+      <UFOcards/>
+    </div>
+  )
+}
+
+export default Dannytest

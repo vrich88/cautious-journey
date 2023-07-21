530c0acbe611a5eadb057df19718886e82b8cb8b
Josh <jsohua.butts4477@gmail.com>
2023-07-20 21:48:53 -0500

    homepage styles and images

---
+++ client/src/assets/data/data.js
@@ -0,0 +1,10 @@
+import PFP from "../images/Cinder525x350.jpg"
+import TFT from "../images/eclipse1.gif"
+
+export const plastic = {
+    image: PFP
+ }
+ export const Cgif = {
+    image: TFT
+ }
+ 

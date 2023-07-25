4c667bf312b024d5ebc3db84857b22946a4b54fc
vrich88 <mvrichards@icloud.com>
2023-07-25 01:23:44 -0500

    alt * method with path.resolve

--- server/server.js
+++ server/server.js
@@ -20,11 +20,11 @@ app.use(express.json());
 // testing
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build','index.html'));
+  res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
 })
 
 // this part works local host
